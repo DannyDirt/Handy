20da0129197598dd13d4a1aefcfae41f6e4fc275
DannyDirt <62881832+DannyDirt@users.noreply.github.com>
2020-04-06 13:02:44 +0200

    Add files via upload
    first try

---
+++ double_img_in_dir.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import os.path
+
+
+
+foldername = #os.path.join('/Users/danielruso/PycharmProjects/NEW/100')
+
+found_files = glob.glob(os.path.join(foldername,'*.jpg'), recursive=True)
+
+for root, _, files in os.walk(foldername):
+    for f in files:
+        fullpath = os.path.join(root, f)
+        if f.endswith(' 2.jpg'):
+            os.remove(fullpath)
+        elif f.endswith(' 3.jpg'):
+            os.remove(fullpath)
+        elif f.endswith(' 4.jpg'):
+            os.remove(fullpath)
+
